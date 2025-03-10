d47361d7ef0ea4d2a66b921bcb19f48bb316bbdc
Reggie Wade <reggiewade@mac.boisestate.edu>
2025-03-09 18:51:12 -0600

    added functionality to NFA.java

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class NFA implements NFAInterface {
     private Set<Character> sigma;   //Alphabet
-    private Map<Character, NFAState> states;
+    private Map<String, NFAState> states;
     private NFAState startState;
     private Set<NFAState> finalStates;
 
@@ -16,5 +16,53 @@ public class NFA implements NFAInterface {
         finalStates = new LinkedHashSet<>();
     }
 
-    
+    @Override
+    public boolean addState(String name) {
+        if (states.containsKey(name)) return false;
+        states.put(name, new NFAState(name));
+        return true;
+    }
+
+    @Override
+    public boolean setFinal(String name) {
+        NFAState state = states.get(name);
+        if (state == null) return false;
+        finalStates.add(state);
+        return true;
+    }
+
+    @Override
+    public boolean setStart(String name) {
+        NFAState state = states.get(name);
+        if (state == null) return false;
+        startState = state;
+        return true;
+    }
+
+    @Override
+    public void addSigma(char symbol) {
+        sigma.add(symbol);
+    }
+
+    @Override
+    public Set<Character> getSigma() {
+        return sigma;
+    }
+
+    @Override
+    public State getState(String name) {
+        return states.get(name);
+    }
+
+    @Override
+    public boolean isFinal(String name) {
+        NFAState state = states.get(name);
+        return state != null && finalStates.contains(state);
+    }
+
+    @Override
+    public boolean isStart(String name) {
+        return startState != null && startState.getName().equals(name);
+    }
+
 }
